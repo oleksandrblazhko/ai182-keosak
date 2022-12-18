578f88d4bcbf5e302cedf6f0054bb3eb81b31107
Keosak Artem <73271963+tenn5@users.noreply.github.com>
2022-12-18 02:17:59 +0200

    Update get_data_secure.sql

--- Laboratory-work-8/get_data_secure.sql
+++ Laboratory-work-8/get_data_secure.sql
@@ -1,12 +1,12 @@
 DROP FUNCTION get_data(character varying);
 CREATE OR REPLACE FUNCTION get_data (name_ varchar)
 RETURNS TABLE
-(p_id INTEGER, name_ VARCHAR, bd date)
+(p_id INTEGER, name VARCHAR, bd date)
 AS $$
 DECLARE
 	str VARCHAR;
 BEGIN
-	str := 'SELECT * from car where name = $1';
+	str := 'SELECT * from worker where name = $1';
 	RAISE NOTICE 'Query=%',str;
 	RETURN QUERY EXECUTE str USING name_;
 END;
